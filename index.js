a300053e25209128a997a10309de5137c362386e
melissa sidisaid <milyssasidisaid@gmail.com>
2023-07-21 23:28:10 +0200

    finish with this assigment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+let msg = [];
+const names = ["Charlie", "Samip", "Ali"];
+const event = "birthday";
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    msg.push(message);
+  }
+  return msg;
+}
+function countDown (i){
+
+    while (i >= 0) {
+      console.log(i);
+       i--;
+    }
+    }
+    countDown(10);
